06139d48f10e38703ec4f9fe603c7ececa292164
Jacob Northey <jacob@lasalletech.com>
2011-10-29 16:05:40 +0000

    [maven-release-plugin]  copy for tag openfast-1.0.3
    git-svn-id: https://svn.code.sf.net/p/openfast/code/tags/openfast-1.0.3@242 e6ee02d6-d133-0410-bd08-f9337e8129a4

---
+++ openfast-1.0.1/src/main/java/org/openfast/examples/OpenFastExample.java
@@ -0,0 +1,55 @@
+package org.openfast.examples;
+
+import java.io.File;
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+public class OpenFastExample {
+    protected static final String HELP = "help";
+    protected static final String MESSAGE_TEMPLATE_FILE = "template";
+    protected static final String PORT = "port";
+    protected static final String PROTOCOL = "protocol";
+    protected static final String HOST = "host";
+    protected static final String INTERFACE = "interface";
+    protected static final String ERROR = "error";
+    protected static final String NAMESPACE_AWARENESS = "ns";
+    protected static final String OUTPUT_FILE = "output";
+    protected static final String TRACE = "trace";
+    protected static final String FAST_DATA_FILE = "data";
+    protected static final String XML_DATA_FILE = "xml";
+
+    protected static CommandLine parseCommandLine(String name, String[] args, Options options) {
+        try {
+            BasicParser parser = new BasicParser();
+            return parser.parse(options, args);
+        } catch (ParseException e) {
+            displayHelp(name, options);
+            return null;
+        }
+    }
+
+    protected static void displayHelp(String name, Options options) {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp(name, options);
+        System.exit(1);
+    }
+
+    protected static int getInteger(CommandLine cl, String option) {
+        try {
+            return Integer.parseInt(cl.getOptionValue(option));
+        } catch (NumberFormatException e) {
+            System.out.println("The required parameter \"" + option + "\" must be an integer.");
+            System.exit(1);
+            return 0;
+        }
+    }
+
+    protected static File getFile(CommandLine cl, String option) {
+        Assert.assertTrue(cl.hasOption(option), "The required parameter \"" + option + "\" is missing.");
+        File file = new File(cl.getOptionValue(option));
+        return file;
+    }
+}
---
+++ openfast-1.0.1/src/main/java/org/openfast/examples/decoder/Main.java
@@ -0,0 +1,58 @@
+package org.openfast.examples.decoder;
+
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Options;
+import org.openfast.examples.Assert;
+import org.openfast.examples.OpenFastExample;
+
+public class Main extends OpenFastExample {
+    private static Options options = new Options();
+    
+    static {
+        options.addOption("?", "help", false, "Displays this message");
+        options.addOption("d", FAST_DATA_FILE, true, "FAST encoded data file");
+        options.addOption("e", ERROR, false, "Show stacktrace information");
+        options.addOption("n", NAMESPACE_AWARENESS, false, "Enables namespace awareness");
+        options.addOption("t", MESSAGE_TEMPLATE_FILE, true, "Message template definition file");
+        options.addOption("v", TRACE, false, "Trace");
+    }
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        CommandLine cl = parseCommandLine("consumer", args, options);
+        if (cl.hasOption("help")) {
+            displayHelp("consumer", options);
+        }
+        boolean showStacktrace = cl.hasOption(ERROR);
+        File templatesFile = null;
+        File dataFile = null;
+        try {
+            dataFile = getFile(cl, FAST_DATA_FILE);
+            Assert.assertTrue(dataFile.exists(), "The fast data file \"" + dataFile.getAbsolutePath() + "\" does not exist.");
+            Assert.assertTrue(!dataFile.isDirectory(), "The fast data file \"" + dataFile.getAbsolutePath() + "\" is a directory.");
+            Assert.assertTrue(dataFile.canRead(), "The fast data file \"" + dataFile.getAbsolutePath() + "\" is not readable.");
+            templatesFile = getFile(cl, MESSAGE_TEMPLATE_FILE);
+            Assert.assertTrue(templatesFile.exists(), "The template definition file \"" + templatesFile.getAbsolutePath() + "\" does not exist.");
+            Assert.assertTrue(!templatesFile.isDirectory(), "The template definition file \"" + templatesFile.getAbsolutePath() + "\" is a directory.");
+            Assert.assertTrue(templatesFile.canRead(), "The template definition file \"" + templatesFile.getAbsolutePath() + "\" is not readable.");
+        } catch (AssertionError e) {
+            System.out.println(e.getMessage());
+            displayHelp("consumer", options);
+        }
+        FastMessageDecoder consumer = new FastMessageDecoder(dataFile, templatesFile, cl.hasOption(NAMESPACE_AWARENESS));
+        if (cl.hasOption(TRACE))
+            consumer.setTraceEnabled();
+        try {
+            consumer.start();
+        } catch (IOException e) {
+            if (showStacktrace)
+                e.printStackTrace();
+            System.out.println("An IO error occurred while consuming messages: " + e.getMessage());
+            System.exit(1);
+        }
+    }
+}
